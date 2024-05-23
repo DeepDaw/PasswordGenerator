8fcccd91ba7e0fe81ac9d7f69ddcf38ff2e0e63a
DeepDaw <deep.daw84@gmail.com>
2024-05-23 18:50:58 +0530

    First Project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, useCallback } from 'react'
+import ChangePass from './componnent/ChangePass'
+
+
+
+function App() {
+  
+  return (
+    <>
+     <ChangePass/>
+    </>
+  )
+}
+
+export default App
